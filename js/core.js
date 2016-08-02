c262c25fe556735f364d4f8244b5b1c5ded4889b
peter <gilbertjuly@gmail.com>
2016-08-02 23:18:04 +0800

    renew

---
+++ js/core.js
@@ -0,0 +1,9 @@
+/**
+ * @author Paul
+ */
+$(document).ready(function(){
+    $(".typed-element").typed({
+        strings: ["This is a paragraph that is being typed by the typed.js jQuery plugin."],
+        typeSpeed: 50
+      });
+});
