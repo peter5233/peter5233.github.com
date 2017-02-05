d468eb638ca6a3c7ab957fe44eca44f606659460
peter5233 <332876370@qq.com>
2017-02-05 23:10:50 +0800

    clear

--- js/core.js
+++
@@ -1,9 +0,0 @@
-/**
- * @author Paul
- */
-$(document).ready(function(){
-    $(".typed-element").typed({
-        strings: ["This is a paragraph that is being typed by the typed.js jQuery plugin."],
-        typeSpeed: 50
-      });
-});
